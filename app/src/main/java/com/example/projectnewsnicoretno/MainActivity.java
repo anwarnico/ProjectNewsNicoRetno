0489de3ebd1bfc75ce6731375dbe9de98df1b4aa
Nico <U070439@bca.co.id>
2021-10-21 12:02:22 +0700

    Login Page (Belum pakai API)

--- app/src/main/java/com/example/projectnewsnicoretno/MainActivity.java
+++ app/src/main/java/com/example/projectnewsnicoretno/MainActivity.java
@@ -10,5 +10,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, new LoginFragment()).commitNow();
+        }
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        int count = getSupportFragmentManager().getBackStackEntryCount();
+
+        if (count == 0) {
+            super.onBackPressed();
+        }
+        else {
+            getSupportFragmentManager().popBackStack();
+        }
+
     }
 }
